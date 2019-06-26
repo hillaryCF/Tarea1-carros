2dfebe94809b2686ac48af4be88e2d97b98b135d
unknown <hillaryy238@gmail.com>
2019-06-26 16:36:24 -0600

    metodos: GET, POST, DELETE y PUT de cars

---
+++ carros/backend/controllers/car.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const brands = require('../inventories/cars.json');
+const {
+  end,
+  getData,
+  throwErr
+} = require('../controllers/funcion.js');
+
+function GET(req,res){
+
+};
+function POST(req,res){
+
+};
+function DELETE(req,res){
+
+};
+function PUT(req,res){
+
+};
+module.exports = {
+  GET,
+  POST,
+  DELETE,
+  PUT
+}
